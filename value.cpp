f227efa084e27d949d35c8faca84783bc34372fe
Shristi2919 <115880739+Shristi2919@users.noreply.github.com>
2022-10-23 23:12:01 +0530

    Create value.cpp

---
+++ value.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+  cout<<"This programme is created by shristi shreya";
+  int a = 4;
+  int b = 6;
+  cout<<"The value of a is "<<a>>".The value of b is "<<b>>;
+  return 0;
+}
